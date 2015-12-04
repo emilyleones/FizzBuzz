427d1fbed7e6bb8840c9c3f83193738e1b102634
emilyleones <leonesemily@gmail.com>
2015-12-04 01:50:29 -0600

    Refactored tests.

--- test/FizzBuzzTest.java
+++ test/FizzBuzzTest.java
@@ -9,16 +9,19 @@ import static org.junit.Assert.*;
 public class FizzBuzzTest {
     private FizzBuzz fizzBuzz;
 
+    @Before
+    public void setUp() {
+        fizzBuzz = new FizzBuzz(1, 100);
+    }
+
     @Test
     public void shouldReturnOneAsSecondElement() {
-        fizzBuzz = new FizzBuzz(1, 100);
         List<Integer> testList = Arrays.asList(1);
         assertEquals(testList.get(0), fizzBuzz.list().get(0));
     }
 
     @Test
     public void shouldReturnTwoAsSecondElement() {
-        fizzBuzz = new FizzBuzz(1, 100);
         List<Integer> testList = Arrays.asList(1, 2);
         assertEquals(testList.get(1), fizzBuzz.list().get(1));
     }
