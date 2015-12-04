ea6a5e5931453f82dc0a40a42746816415f8e787
emilyleones <leonesemily@gmail.com>
2015-12-04 01:49:17 -0600

    The second element returned is 2.

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,7 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FizzBuzz {
 
-    public int output() {
-        return 1;
+    private int start;
+    private int end;
+
+    public FizzBuzz(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public List<Integer> list() {
+        List<Integer> fizzBuzzList = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            fizzBuzzList.add(i);
+        }
+        return fizzBuzzList;
     }
 }
--- test/FizzBuzzTest.java
+++ test/FizzBuzzTest.java
@@ -1,4 +1,8 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -6,9 +10,16 @@ public class FizzBuzzTest {
     private FizzBuzz fizzBuzz;
 
     @Test
-    public void shouldReturnOne() {
-        fizzBuzz = new FizzBuzz();
-        assertEquals(1, fizzBuzz.output());
+    public void shouldReturnOneAsSecondElement() {
+        fizzBuzz = new FizzBuzz(1, 100);
+        List<Integer> testList = Arrays.asList(1);
+        assertEquals(testList.get(0), fizzBuzz.list().get(0));
     }
 
+    @Test
+    public void shouldReturnTwoAsSecondElement() {
+        fizzBuzz = new FizzBuzz(1, 100);
+        List<Integer> testList = Arrays.asList(1, 2);
+        assertEquals(testList.get(1), fizzBuzz.list().get(1));
+    }
 }
